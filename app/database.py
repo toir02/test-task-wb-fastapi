139938ff5d0fe0c522ab08b8cb122885ccff282f
toir02 <wend8765@gmail.com>
2024-04-12 00:58:48 +0300

    changed import from another module

--- app/database.py
+++ app/database.py
@@ -1,8 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import (
-    sessionmaker,
-    declarative_base
-)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 from config import (
     DB_USERNAME,
