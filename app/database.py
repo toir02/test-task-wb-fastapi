39213636a6766ba739dcbb46e2cef5c8c19f9ffd
toir02 <wend8765@gmail.com>
2024-04-12 00:51:27 +0300

    added config for db

--- app/database.py
+++ app/database.py
@@ -1,3 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import (
+    sessionmaker,
+    declarative_base
+)
+
 from config import (
     DB_USERNAME,
     DB_PASSWORD,
@@ -7,3 +13,20 @@ from config import (
 )
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False
+)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
